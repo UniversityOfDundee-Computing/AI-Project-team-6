8324041812a281e67128e1da5b23598ef057e336
Adam Mathieson <adam@genav.ch>
2022-03-18 00:19:56 +0000

    + GUI Cleanup

--- script/main.js
+++ script/main.js
@@ -6,7 +6,7 @@ const GRID_CELLS_Y = 32;
 
 // Global States
 let GRID_DATA = [];
-let GRID_MODE = "OBS"; // OBS = obstruction, WEI = weight, SRT = start loc, TAR = target loc, TRR = rem target
+let GRID_MODE = "OBS"; // OBS = obstruction, WEI = weight, DAM = damaged, ERA = eraser
 let GRID_Current_Weight = 0;
 let GRID_Image_Data = null;
 let GRID_Targets = [];
@@ -22,33 +22,11 @@ renderGrid(); // Initial call
 
 
 // Event Handlers
-CANVAS.onclick = (event) => {
-    const x = Math.round(event.offsetX / (CANVAS.offsetWidth) * GRID_CELLS_X);
-    const y = Math.round(event.offsetY / (CANVAS.offsetHeight) * GRID_CELLS_Y);
-    switch (GRID_MODE) {
-        case "SRT":
-            GRID_StartPos = {x: x, y: y};
-            break;
-        case "TAR":
-            GRID_Targets.push({x: x, y: y});
-            break;
-        case "TRR":
-            let index = GRID_Targets.findIndex((i) => {
-                return i.x === x && i.y === y
-            });
-            if (index !== -1) {
-                GRID_Targets.splice(index, 1);
-            }
-            break;
-        case "OBS":
-            GRID_DATA[y][x] = -1;
-            break;
-        case "WEI":
-            GRID_DATA[y][x] = (GRID_MODE === "OBS" ? -1 : GRID_Current_Weight);
-            break;
-    }
+CANVAS.onclick = addPoint;
 
-    renderGrid();
+CANVAS.onmousemove = (event) => {
+    if (event.buttons === 1)
+        addPoint(event);
 }
 
 document.getElementById("btn_clear").onclick = (_) => {
@@ -62,22 +40,22 @@ document.getElementById("btn_clear").onclick = (_) => {
     renderGrid();
 }
 
-document.getElementById("btn_Obs").onclick = (_) => {
+document.getElementById("btn_markObstruction").onclick = (_) => {
     GRID_MODE = "OBS";
+    updateRadios();
+}
+document.getElementById("btn_eraser").onclick = (_) => {
+    GRID_MODE = "ERA";
+    updateRadios();
+}
+document.getElementById("btn_markDamaged").onclick = (_) => {
+    GRID_MODE = "DAM";
+    updateRadios();
 }
 
-document.getElementById("btn_start").onclick = (_) => {
-    GRID_MODE = "SRT";
-}
-document.getElementById("btn_target").onclick = (_) => {
-    GRID_MODE = "TAR";
-}
-document.getElementById("btn_rem_target").onclick = (_) => {
-    GRID_MODE = "TRR";
-}
-
-document.getElementById("btn_CellW").onclick = (_) => {
+document.getElementById("btn_setCellWeight").onclick = (_) => {
     GRID_MODE = "WEI";
+    updateRadios();
     GRID_Current_Weight = parseFloat(
         prompt("Please enter a float from 0 to 1 representing the weight of the cell.")
     );
@@ -160,45 +138,12 @@ function renderGrid() {
             CONTEXT.fillRect(x * (CANVAS.width / GRID_CELLS_X), y * (CANVAS.height / GRID_CELLS_Y),
                 (CANVAS.width / GRID_CELLS_X), (CANVAS.height / GRID_CELLS_Y));
 
-            if (GRID_StartPos.x === x && GRID_StartPos.y === y)
-                // CONTEXT.strokeStyle = "rgba(0,255,0,0.5)";
-                CONTEXT.strokeStyle = "rgba(0,0,0,0.5)";
-            else if (GRID_Targets.findIndex((i) => {
-                return i.x === x && i.y === y
-            }) !== -1)
-                // CONTEXT.strokeStyle = "rgba(0,0,255,1)";
-                CONTEXT.strokeStyle = "rgba(0,0,0,0.5)";
-            else
-                // CONTEXT.strokeStyle = "rgba(255,0,0,0.5)";
-                CONTEXT.strokeStyle = "rgba(0,0,0,0.5)";
+            CONTEXT.strokeStyle = "rgba(0,0,0,0.5)";
 
             CONTEXT.strokeRect(x * (CANVAS.width / GRID_CELLS_X), y * (CANVAS.height / GRID_CELLS_Y),
                 (CANVAS.width / GRID_CELLS_X), (CANVAS.height / GRID_CELLS_Y));
         }
     }
-    generateOutput();
-}
-
-/**
- * Generate the CLI outputs
- */
-function generateOutput() {
-    let GRID_TargetsCLI = "";
-    let GRID_TargetsList = "";
-    GRID_Targets.forEach((el) => {
-        GRID_TargetsList += `<li>${el.x};${el.y}</li>`
-    });
-
-    GRID_TargetsCLI = GRID_TargetsList.replaceAll("</li><li>", ",")
-        .replaceAll("<li>", "").replaceAll("</li>", "");
-
-    document.getElementById("output").innerHTML =
-        `<p>Start Location: ${GRID_StartPos.x};${GRID_StartPos.y}</p>` +
-        "Points to visit:" +
-        "<ul>" +
-        GRID_TargetsList +
-        "</ul>" +
-        `<code>python ./main.py map.cfg ${GRID_StartPos.x};${GRID_StartPos.y} ${GRID_TargetsCLI}</code>`;
 }
 
 // Function to download data to a file - https://stackoverflow.com/a/30832210
@@ -218,4 +163,35 @@ function downloadFile(data, filename, type) {
             window.URL.revokeObjectURL(url);
         }, 0);
     }
+}
+
+
+function addPoint(event) {
+    const x = Math.round(event.offsetX / (CANVAS.offsetWidth) * GRID_CELLS_X);
+    const y = Math.round(event.offsetY / (CANVAS.offsetHeight) * GRID_CELLS_Y);
+
+    if (x >= 0 && x < GRID_CELLS_X && y >= 0 && y < GRID_CELLS_Y) {
+        switch (GRID_MODE) {
+            case "OBS":
+                GRID_DATA[y][x] = -1;
+                break;
+            case "WEI":
+                GRID_DATA[y][x] = (GRID_MODE === "OBS" ? -1 : GRID_Current_Weight);
+                break;
+            case "DAM":
+                GRID_DATA[y][x] = 0.5;
+                break;
+            case "ERA":
+                GRID_DATA[y][x] = 0;
+                break;
+        }
+    }
+    renderGrid();
+}
+
+function updateRadios() {
+    document.getElementById("markObstruction").checked = (GRID_MODE === "OBS");
+    document.getElementById("setCellWeight").checked = (GRID_MODE === "WEI");
+    document.getElementById("markDamaged").checked = (GRID_MODE === "DAM");
+    document.getElementById("eraser").checked = (GRID_MODE === "ERA");
 }
