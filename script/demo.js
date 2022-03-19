a79d484b15e4bf1a3bc2627021ad3ce125900de3
Adam Mathieson <adam@genav.ch>
2022-03-19 16:24:33 +0000

    + working buttons

--- script/demo.js
+++ script/demo.js
@@ -1,13 +1,32 @@
-let START_LOCATION = new Location(23, 7);
-let TARGET_LOCATIONS = [new Location(45, 28)];
+let START_LOCATION = new Location(0, 0);
+let TARGET_LOCATIONS = [];
+let GRID_MODE = "SRT";
 
 
 document.getElementById("goBtn").onclick = (_) => {
-    findPath(START_LOCATION, TARGET_LOCATIONS, document.getElementById("method").value);
-
     isVisualisationOn = document.getElementById("isViz").checked;
     isVisualisationDelayOn = document.getElementById("isDelay").checked;
     visualisationDelayAmount = document.getElementById("delayAmount").value;
+
+    findPath(START_LOCATION, TARGET_LOCATIONS, document.getElementById("method").value);
+}
+
+document.getElementById("btn_markStart").onclick = (_) => {
+    GRID_MODE = "SRT";
+}
+
+document.getElementById("btn_markTarget").onclick = (_) => {
+    GRID_MODE = "TAR";
+}
+document.getElementById("btn_eraser").onclick = (_) => {
+    GRID_MODE = "ERA";
+}
+
+CANVAS.onclick = addPoint;
+
+CANVAS.onmousemove = (event) => {
+    if (event.buttons === 1)
+        addPoint(event);
 }
 
 // Test Code
@@ -17,3 +36,37 @@ fetch("map.cfg").then((d) => {
         importFrom2dArr(data);
     });
 })
+
+
+function addPoint(event) {
+    const x = Math.round(event.offsetX / (CANVAS.offsetWidth) * GRID_CELLS_X);
+    const y = Math.round(event.offsetY / (CANVAS.offsetHeight) * GRID_CELLS_Y);
+
+    if (x >= 0 && x < GRID_CELLS_X && y >= 0 && y < GRID_CELLS_Y) {
+        switch (GRID_MODE) {
+            case "SRT":
+                if (GRID_DATA[y][x].v !== -1) {
+                    GRID_DATA[y][x].c = 2;
+                    GRID_DATA[START_LOCATION.y][START_LOCATION.x].c = -1;
+                    START_LOCATION = new Location(x, y);
+                }
+                break;
+            case "TAR":
+                if (GRID_DATA[y][x].v !== -1) {
+                    GRID_DATA[y][x].c = 3;
+                    TARGET_LOCATIONS.push(new Location(x, y));
+                }
+                break;
+            case "ERA":
+                if (GRID_DATA[y][x].v !== -1) {
+                    GRID_DATA[y][x].c = -1;
+                    const index = TARGET_LOCATIONS.indexOf(new Location(x, y));
+                    if (index > -1) {
+                        TARGET_LOCATIONS.splice(index, 1);
+                    }
+                }
+                break;
+        }
+    }
+    renderGrid();
+}
